b12bf8f99edbbacb1bda637e092b919d2033818e
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-20 21:04:11 +0200

    Docs for deki-esp32-integration from deki-editor d152da0

--- navtreedata.js
+++ navtreedata.js
@@ -31,8 +31,11 @@ var NAVTREE =
       [ "ESP32SerialSetup", "index.html#c-esp32serialsetup", null ]
     ] ],
     [ "Changelog", "index.html#autotoc_md3", [
-      [ "0.15.0", "index.html#autotoc_md4", null ],
-      [ "Fixed", "index.html#autotoc_md5", null ]
+      [ "Unreleased", "index.html#autotoc_md4", null ],
+      [ "Changed", "index.html#autotoc_md5", null ],
+      [ "Fixed", "index.html#autotoc_md6", null ],
+      [ "0.16.0", "index.html#autotoc_md7", null ],
+      [ "Changed", "index.html#autotoc_md8", null ]
     ] ]
   ] ]
 ];
